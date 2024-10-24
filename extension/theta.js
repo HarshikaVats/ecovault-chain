8184533396b365b69672961aeaba08a194cd4d43
VishalVats <vishalvats2000@gmail.com>
2024-10-24 09:53:45 +0530

    Extension to be used by buyers

---
+++ extension/theta.js
@@ -0,0 +1,6 @@
+export const NODE_SERVER_URL =
+  "https://node-server-sainipratap-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com";
+export const ML_MODEL_URL =
+  "https://ml-model-sainipratap-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com";
+export const THETA_URL =
+  "https://theta-server-sainipratap-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com";
